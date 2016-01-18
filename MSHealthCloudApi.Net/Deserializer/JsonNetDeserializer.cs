5f7a54298bd56243dac3ce5c823fd7c0997f8c7c
Dick van den Brink <d_vandenbrink@outlook.com>
2016-01-19 00:45:29 +0100

    Added Serializer code

---
+++ MSHealthCloudApi.Net/Deserializer/JsonNetDeserializer.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace MSHealthCloudApi.Net.Deserializer
+{
+    public class JsonNetDeserializer : IDeserializer
+    {
+        public string DateFormat
+        {
+            get; set;
+        }
+
+        public string Namespace
+        {
+            get; set;
+        }
+
+        public string RootElement
+        {
+            get; set;
+        }
+
+        public T Deserialize<T>(IRestResponse response)
+        {
+            var _serializer = new Newtonsoft.Json.JsonSerializerSettings
+            {
+                MissingMemberHandling = MissingMemberHandling.Ignore,
+                NullValueHandling = NullValueHandling.Include,
+                DefaultValueHandling = DefaultValueHandling.Include
+            };
+            return JsonConvert.DeserializeObject<T>(response.Content, _serializer);
+        }
+    }
+}
---
+++ MSHealthCloudApi.Net/MSHealthCloudApi.cs
@@ -0,0 +1,23 @@
+﻿using MSHealthCloudApi.Net.Deserializer;
+using MSHealthCloudApi.Net.Serializers;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSHealthCloudApi.Net
+{
+    public class MSHealthCloudApi
+    {
+        private RestClient client;
+        public MSHealthCloudApi()
+        {
+            client = new RestClient();
+            client.AddHandler("*", new JsonNetDeserializer());
+            var req = new RestRequest();
+            req.JsonSerializer = new JsonNetSerializer();
+        }
+    }
+}
