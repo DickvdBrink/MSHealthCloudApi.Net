3da008936b208458725561bfdea2ad6ad97556bc
Dick van den Brink <d_vandenbrink@outlook.com>
2016-01-20 20:09:28 +0100

    Basic MSHealthCloudApi Client

--- MSHealthCloudApi.Net/MSHealthCloudApi.cs
+++ MSHealthCloudApi.Net/MSHealthCloudApi.cs
@@ -1,4 +1,5 @@
 ﻿using MSHealthCloudApi.Net.Deserializer;
+using MSHealthCloudApi.Net.Model;
 using MSHealthCloudApi.Net.Serializers;
 using RestSharp;
 using System;
@@ -11,13 +12,107 @@ namespace MSHealthCloudApi.Net
 {
     public class MSHealthCloudApi
     {
+        private const string Scopes = "mshealth.ReadDevices mshealth.ReadActivityHistory mshealth.ReadActivityLocation";
+        private const string BaseHealthUri = "https://api.microsofthealth.net";
+
         private RestClient client;
         public MSHealthCloudApi()
         {
-            client = new RestClient();
+            client = new RestClient(BaseHealthUri);
             client.AddHandler("*", new JsonNetDeserializer());
-            var req = new RestRequest();
-            req.JsonSerializer = new JsonNetSerializer();
         }
+
+        #region Activities
+
+        public ActivitiesResponse GetActivities()
+        {
+            return performRequest<ActivitiesResponse>("/v1/me/Activities");
+        }
+
+        #endregion
+
+        #region Devices
+
+        public List<DeviceProfile> GetDevicesProfile()
+        {
+            return performRequest<List<DeviceProfile>>("/v1/me/Devices");
+        }
+
+        public Task<List<DeviceProfile>> GetDevicesProfileAsync()
+        {
+            return performRequestAsync<List<DeviceProfile>>("/v1/me/Devices");
+        }
+
+        public DeviceProfile GetDeviceProfile(string deviceId)
+        {
+            RestRequest request = createRequest("/v1/me/Devices/{deviceId}");
+            request.AddUrlSegment("deviceId", deviceId);
+            return performRequest<DeviceProfile>(request);
+        }
+
+        public Task<DeviceProfile> GetDeviceProfileAsync(string deviceId)
+        {
+            RestRequest request = createRequest("/v1/me/Devices/{deviceId}");
+            request.AddUrlSegment("deviceId", deviceId);
+            return performRequestAsync<DeviceProfile>(request);
+        }
+
+        #endregion
+
+        #region UserProfile
+
+        public UserProfile GetUserProfile()
+        {
+            return performRequest<UserProfile>("/v1/me/Profile");
+        }
+
+        public Task<UserProfile> GetUserProfileAsync()
+        {
+            return performRequestAsync<UserProfile>("/v1/me/Profile");
+        }
+
+        #endregion
+
+        #region Summaries
+
+        #endregion
+
+        #region RequestHelpers
+
+        private RestRequest createRequest(string resource)
+        {
+            RestRequest request = new RestRequest(resource);
+            request.AddHeader("Authorization", $"bearer { null }");
+            request.JsonSerializer = new JsonNetSerializer();
+            return request;
+        }
+
+        private T performRequest<T>(string resource)
+            where T : new()
+        {
+            return performRequest<T>(createRequest(resource));
+        }
+
+        private T performRequest<T>(RestRequest request)
+            where T : new()
+        {
+            IRestResponse<T> response = client.Get<T>(request);
+            return response.Data;
+        }
+
+        private Task<T> performRequestAsync<T>(string resource)
+            where T : new()
+        {
+            return performRequestAsync<T>(createRequest(resource));
+        }
+
+        private Task<T> performRequestAsync<T>(RestRequest request)
+            where T : new()
+        {
+            Task<T> response = client.GetTaskAsync<T>(request);
+            return response;
+        }
+
+        #endregion
     }
 }
