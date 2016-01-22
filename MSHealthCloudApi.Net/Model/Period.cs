be14118c672ff8f35031690e9b8c65f297c2b4d7
Dick van den Brink <d_vandenbrink@outlook.com>
2016-01-22 16:51:43 +0100

    Add Enums for Period and SplitDistance

---
+++ MSHealthCloudApi.Net/Model/Period.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSHealthCloudApi.Net.Model
+{
+    public enum Period
+    {
+        Hourly,
+        Daily
+    }
+}
